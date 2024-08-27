20f4d3e0f23c3788b42968519f42107ca97d27c2
Ansari-Mehsar <135012520+Ansari-Mehsar@users.noreply.github.com>
2024-08-27 22:02:09 +0530

    Add files via upload

---
+++ python_day3_statement_assigment 3.py
@@ -0,0 +1,23 @@
+# 3. Write a Program to Convert Kilometers to Miles
+
+# Input:Enter distance in Kilometers
+
+kilometers = float(input("Enter distance in kilometers: "))
+
+# Conversion factor: 1 kilometer = 0.621371 miles
+
+conversion_factor = 0.621371
+
+# Convert kilometers to miles
+
+miles = kilometers * conversion_factor
+
+# Printing the result
+print(f"{kilometers} kilometers is equal to {miles} miles.")
+
+#Output
+"""
+Enter distance in kilometers: 5
+5.0 kilometers is equal to 3.106855 miles.
+
+"""
---
+++ python_day3_statement_assigment 4.py
@@ -0,0 +1,19 @@
+# 4. Find the Simple Interest on Rs. 200 for 5 years at 5% per year.
+
+
+# Given data for Simple Interest calculation
+principal = 200  # Principal amount in Rs.
+rate_of_interest = 5  # Rate of interest per year in percentage
+time_period = 5  # Time period in years
+
+# Simple Interest formula: SI = (Principal * Rate * Time) / 100
+simple_interest = (principal * rate_of_interest * time_period) / 100
+
+# printing the result
+print(f"The Simple Interest is: Rs. {simple_interest}")
+
+#Output
+"""
+The Simple Interest is: Rs. 50.0
+
+"""
