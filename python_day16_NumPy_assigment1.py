293e8bd24c9bdbbe96d768346bc96d09ed4d2895
Ansari-Mehsar <135012520+Ansari-Mehsar@users.noreply.github.com>
2024-09-17 18:09:07 +0530

    Add files via upload

---
+++ python_day16_NumPy_assigment1.py
@@ -0,0 +1,20 @@
+#1. Convert the below list into numpy array then display the array
+
+# Input: my_list = [1, 2, 3, 4, 5] 
+
+import numpy as np
+
+# Input list
+my_list = [1, 2, 3, 4, 5]
+
+# Convert list to NumPy array
+my_array = np.array(my_list)   # np is commonly used when importing NumPy
+
+# Printing the result
+print(my_array)
+
+#ouput
+"""
+[1 2 3 4 5]
+"""
+
